45f4bf771f01d8257cf3ff68d0f3107771718e36
Alex Duchemin <cbx92@hotmail.fr>
2021-05-18 17:17:41 +0200

    Add route get offers

--- frontend/src/components/JobsAvailable.js
+++ frontend/src/components/JobsAvailable.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Col, Row, Button } from "reactstrap";
 import NavBar from './NavBar'
 import OpenInNewIcon from '@material-ui/icons/OpenInNew';
@@ -11,83 +11,131 @@ import '../App.css';
 
 
 function JobsAvailable() {
-  return (
-  <div className="section">
-        <NavBar />
-  <Container style={{marginTop: '100px'}}>
-  <Container>
-    <Row className="mb-4 d.flex">
-    <Col className="flex-row mr-1">
-        <input className="searchBar" type="search" placeholder="Rechercher un poste.."/>
-        <button className="onclickButton" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Tri par date <ExpandMoreIcon/></button>
-        <button className="onclickButton" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Tri par bonus <ExpandMoreIcon/></button>
-    </Col>
-    </Row>
-    <Row>
+
+    const [offers, setOffers] = useState([])
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        const fetchOffers = async () => {
+            setLoading(true)
+            var rawResponse = await fetch('/jobs/get')
+            var response = await rawResponse.json()
+            console.log('reponse du fetch --->', response)
+            setOffers(response.offers)
+            setLoading(false)
+        }
+        fetchOffers()
+    }, [])
+
+    const offersList = offers.map(offer => {
+        return (
             <div className=" col-md-12 cardBackground mb-2">
-            <li className="d-flex flex-column flex-md-row align-items-center justify-content-around">
-                <h2>Développeur Fullstack JS.</h2>
-                <div className="cardInfoBg">
-                    <span className="d-flex justify-content-center">
-                        <div className="cardInfoIcons">
-                        <CalendarTodayIcon fontSize="large"/>
-                            <p>Il y a 12 jours...</p>
+                <li className="d-flex flex-column flex-md-row align-items-center justify-content-around">
+                    <h2>{offer.title}</h2>
+                    <div className="cardInfoBg">
+                        <span className="d-flex justify-content-center">
+                            <div className="cardInfoIcons">
+                                <CalendarTodayIcon fontSize="large" />
+                                <p>{offer.creationDate}</p>
+                            </div>
+                            <div className="cardInfoIcons">
+                                <BusinessCenterIcon fontSize="large" />
+                                <p>{offer.contract}</p>
+                            </div>
+                            <div className="cardInfoIcons">
+                                <PlaceIcon fontSize="large" />
+                                <p>{offer.city}</p>
+                            </div>
+                        </span>
+                    </div>
+                    <h3>{offer.bonusAmount}€</h3>
+                    <Button id="referralButton">Recommander</Button>
+                    <Button id="enlargeButton">
+                        <OpenInNewIcon />
+                    </Button>
+                </li>
+            </div>
+        )
+    })
+
+    return (
+        <div className="section">
+            <NavBar />
+            <Container style={{ marginTop: '100px' }}>
+                <Container>
+                    <Row className="mb-4 d.flex">
+                        <Col className="flex-row mr-1">
+                            <input className="searchBar" type="search" placeholder="Rechercher un poste.." />
+                            <button className="onclickButton" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Tri par date <ExpandMoreIcon /></button>
+                            <button className="onclickButton" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Tri par bonus <ExpandMoreIcon /></button>
+                        </Col>
+                    </Row>
+                    <Row>
+
+                        {offersList}
+
+                        <div className=" col-md-12 cardBackground mb-2">
+                            <li className="d-flex flex-column flex-md-row align-items-center justify-content-around">
+                                <h2>Développeur Fullstack JS.</h2>
+                                <div className="cardInfoBg">
+                                    <span className="d-flex justify-content-center">
+                                        <div className="cardInfoIcons">
+                                            <CalendarTodayIcon fontSize="large" />
+                                            <p>Il y a 12 jours...</p>
+                                        </div>
+                                        <div className="cardInfoIcons">
+                                            <BusinessCenterIcon fontSize="large" />
+                                            <p>CDI</p>
+                                        </div>
+                                        <div className="cardInfoIcons">
+                                            <PlaceIcon fontSize="large" />
+                                            <p>Paris</p>
+                                        </div>
+                                    </span>
+                                </div>
+                                <h3>150€</h3>
+                                <Button id="referralButton">Recommander</Button>
+                                <Button id="enlargeButton">
+                                    <OpenInNewIcon />
+                                </Button>
+                            </li>
                         </div>
-                        <div className="cardInfoIcons">
-                        <BusinessCenterIcon fontSize="large"/>
-                            <p>CDI</p>
+
+                        <div className=" col-md-12 cardBackground mb-2">
+                            <li className="d-flex flex-column flex-md-row align-items-center justify-content-around">
+                                <h2>Commercial itinérant.</h2>
+                                <div className="cardInfoBg">
+                                    <span className="d-flex justify-content-center">
+                                        <div className="cardInfoIcons">
+                                            <CalendarTodayIcon fontSize="large" />
+                                            <p>Il y a 3 jours...</p>
+                                        </div>
+                                        <div className="cardInfoIcons">
+                                            <BusinessCenterIcon fontSize="large" />
+                                            <p>CDD</p>
+                                        </div>
+                                        <div className="cardInfoIcons">
+                                            <PlaceIcon fontSize="large" />
+                                            <p>Paris</p>
+                                        </div>
+                                    </span>
+                                </div>
+                                <h3>150€</h3>
+                                <Button id="referralButton">Recommander</Button>
+                                <Button id="enlargeButton">
+                                    <OpenInNewIcon />
+                                </Button>
+                            </li>
                         </div>
-                        <div className="cardInfoIcons"> 
-                            <PlaceIcon fontSize="large"/>
-                            <p>Paris</p>
-                        </div>
-                    </span>
-                </div>
-                <h3>150€</h3>
-                <Button id="referralButton">Recommander</Button>
-                <Button id="enlargeButton">
-                    <OpenInNewIcon/>
-                </Button>
-            </li>
+                    </Row>
+
+                </Container>
+            </Container>
+
+
+
         </div>
-    </Row>
-
-    <Row>
-            <div className=" col-md-12 cardBackground mb-2">
-            <li className="d-flex flex-column flex-md-row align-items-center justify-content-around">
-                <h2>Commercial itinérant.</h2>
-                <div className="cardInfoBg">
-                    <span className="d-flex justify-content-center">
-                        <div className="cardInfoIcons">
-                        <CalendarTodayIcon fontSize="large"/>
-                            <p>Il y a 3 jours...</p>
-                        </div>
-                        <div className="cardInfoIcons">
-                        <BusinessCenterIcon fontSize="large"/>
-                            <p>CDD</p>
-                        </div>
-                        <div className="cardInfoIcons"> 
-                            <PlaceIcon fontSize="large"/>
-                            <p>Paris</p>
-                        </div>
-                    </span>
-                </div>
-                <h3>150€</h3>
-                <Button id="referralButton">Recommander</Button>
-                <Button id="enlargeButton">
-                    <OpenInNewIcon/>
-                </Button>
-            </li>
-        </div>
-    </Row>
-
-  </Container>
-  </Container>
-
-
-
-  </div>
-  );
+    );
 }
 
 export default JobsAvailable;

41a4a85bf9b7a7bdfa40e93e8bfc899910023f76
Mathieu Domichard <mathieu.domichard@gmail.com>
2021-05-19 15:26:12 +0200

    supression du groupModel

--- models/groups.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const groupsSchema = mongoose.Schema({
-    type : String,
-})
-
-const groupsModel = mongoose.model('groups', groupsSchema)
-
-module.exports = groupsModel

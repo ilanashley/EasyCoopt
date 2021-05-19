67ef4012c379e63bff55dfcba585fde85397f58d
Mathieu Domichard <mathieu.domichard@gmail.com>
2021-05-19 11:54:52 +0200

    photo ajoutee a la page mon profil

--- frontend/src/reducers/type.js
+++ frontend/src/reducers/type.js
@@ -1,6 +1,5 @@
 export default function(typeID = '', action){
     if(action.type == 'addProfileType'){
-        console.log('type dans le reducer -->',action.typeID)
         return action.typeID
     } else {
         return typeID
